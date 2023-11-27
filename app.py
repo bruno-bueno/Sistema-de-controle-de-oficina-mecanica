82b1e9f1e02e07f0ad1fc1af28d57df42f17f6aa
bruno-bueno <bbueno1122@gmail.com>
2023-11-26 23:55:37 -0300

    correção login e templates telas

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ load_dotenv()
 
 app = Flask(__name__)
 
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
@@ -25,13 +27,33 @@ def login():
     from model import Usuarios 
     usuarioModel = Usuarios()  
 
+    if(session):
+        return redirect("/home")
     if request.method == 'POST':
         nome = request.form['nome']
         senha = request.form['senha']
-        user = usuarioModel.autenticarLogin(nome, senha)
 
-        if user:
-            session['id_usuario'] = user['id_usuario']
-            return redirect(url_for('dashboard'))
+        try:
+            user = usuarioModel.autenticarLogin(nome, senha)
+            print(f"usuario: {user[0]}")
+            if user:
+                session['id_usuario'] = user[0]
+                return redirect("/home")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
 
+    return render_template('login.html')
+
+@app.route("/home", methods=['GET', 'POST'])
+def homeUm():
+    print(f"usuario: {session}")
+    if(session):
+        return render_template('index.html')
+    return render_template('login.html')
+
+@app.route("/clientesVeiculos", methods=['GET', 'POST'])
+def clientesVeiculos():
+    print(f"sessão: {session}")
+    if(session):
+        return render_template('clientesVeiculos.html')
     return render_template('login.html')
